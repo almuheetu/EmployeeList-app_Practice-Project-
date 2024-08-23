008e91141e1c0ffeb53538058fb62d0ab898f636
Almuheetu shihab <almuheetu@gmail.com>
2024-08-23 16:28:05 +0600

    fragment, xml , data class alewdy add

---
+++ app/src/main/java/com/example/employeelistapp/Employee.kt
@@ -0,0 +1,9 @@
+package com.example.employeelistapp
+
+data class Employee(
+    val name: String,
+    val age: String,
+    val image: String,
+    val email: String,
+    val address: String
+)
---
+++ app/src/main/java/com/example/employeelistapp/EmployeeFragment.kt
@@ -0,0 +1,4 @@
+package com.example.employeelistapp
+
+class EmployeeFragment {
+}
