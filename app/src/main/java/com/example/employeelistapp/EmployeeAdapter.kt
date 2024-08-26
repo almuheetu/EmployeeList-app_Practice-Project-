6b05efc29f37861c4dc6dd6ec27ae0094e81b946
Almuheetu shihab <almuheetu@gmail.com>
2024-08-26 15:14:39 +0600

    solved all problem

--- app/src/main/java/com/example/employeelistapp/EmployeeAdapter.kt
+++ app/src/main/java/com/example/employeelistapp/EmployeeAdapter.kt
@@ -1,13 +1,13 @@
 package com.example.employeelistapp
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.employeelistapp.databinding.EmployeeFragmentBinding
 import com.example.employeelistapp.databinding.EmployeeListBinding
 
 class EmployeeAdapter(
-    val employeesList: ArrayList<Employee>,
+    private val employeesList: ArrayList<Employee>,
 ) : RecyclerView.Adapter<EmployeeAdapter.ViewHolder>() {
     class ViewHolder(var binding: EmployeeListBinding) : RecyclerView.ViewHolder(binding.root)
 
@@ -20,6 +20,7 @@ class EmployeeAdapter(
         return ViewHolder(binding)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(
         viewHolder: ViewHolder,
         position: Int
@@ -27,7 +28,7 @@ class EmployeeAdapter(
         val employee = employeesList[position]
         viewHolder.binding.employeeName.text = employee.name
         viewHolder.binding.inEmployeeAge.emailText.text = "Age :"
-        viewHolder.binding.inEmployeeAge.emailValue.text = employee.age.toString()
+        viewHolder.binding.inEmployeeAge.emailValue.text = employee.age
         viewHolder.binding.inEmployeeEmail.emailText.text = "Email :"
         viewHolder.binding.inEmployeeEmail.emailValue.text = employee.email
         viewHolder.binding.inEmployeeAddress.emailText.text = "Address :"
--- app/src/main/java/com/example/employeelistapp/MainActivity.kt
+++ app/src/main/java/com/example/employeelistapp/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.employeelistapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.employeelistapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
